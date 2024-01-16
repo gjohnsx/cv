adfd858cb86f89638ef2cc3f91644a09b577577d
gjohnsx <gregjohns@fastmail.com>
2024-01-16 15:15:00 -0500

    updated info

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -35,10 +35,10 @@ export const CommandMenu = ({ links }: Props) => {
 
   return (
     <>
-      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground print:hidden xl:block">
+      <p className="fixed bottom-0 left-0 right-0 hidden p-1 text-sm text-center bg-white border-t border-t-muted text-muted-foreground print:hidden xl:block">
         Press{" "}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>J
+          <span className="text-xs">ctrl</span>j
         </kbd>{" "}
         to open the command menu
       </p>
@@ -46,7 +46,7 @@ export const CommandMenu = ({ links }: Props) => {
         onClick={() => setOpen((open) => !open)}
         variant="outline"
         size="icon"
-        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl print:hidden xl:hidden"
+        className="fixed flex rounded-full shadow-2xl bottom-4 right-4 print:hidden xl:hidden"
       >
         <CommandIcon className="my-6 size-6" />
       </Button>
--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -6,18 +6,28 @@ import {
   CardTitle,
 } from "./ui/card";
 import { Badge } from "./ui/badge";
+import Image from "next/image";
 
 interface Props {
   title: string;
   description: string;
   tags: readonly string[];
   link?: string;
+  logo: any;
 }
 
-export function ProjectCard({ title, description, tags, link }: Props) {
+export function ProjectCard({ title, description, tags, link, logo }: Props) {
   return (
-    <Card className="flex flex-col overflow-hidden border border-muted p-3">
+    <Card className="flex flex-col p-3 overflow-hidden border border-muted">
       <CardHeader className="">
+        <Image
+          alt={"Logo"}
+          src={logo.src}
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
+
         <div className="space-y-1">
           <CardTitle className="text-base">
             {link ? (
@@ -27,12 +37,13 @@ export function ProjectCard({ title, description, tags, link }: Props) {
                 className="inline-flex items-center gap-1 hover:underline"
               >
                 {title}{" "}
-                <span className="size-1 rounded-full bg-green-500"></span>
+                <span className="bg-green-500 rounded-full size-1"></span>
               </a>
             ) : (
               title
             )}
           </CardTitle>
+
           <div className="hidden font-mono text-xs underline print:visible">
             {link?.replace("https://", "").replace("www.", "").replace("/", "")}
           </div>
@@ -41,8 +52,8 @@ export function ProjectCard({ title, description, tags, link }: Props) {
           </CardDescription>
         </div>
       </CardHeader>
-      <CardContent className="mt-auto flex">
-        <div className="mt-2 flex flex-wrap gap-1">
+      <CardContent className="flex mt-auto">
+        <div className="flex flex-wrap gap-1 mt-2">
           {tags.map((tag) => (
             <Badge
               className="px-1 py-0 text-[10px]"
